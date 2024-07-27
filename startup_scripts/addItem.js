5262d6276c6dccf9c0637af5082530d8d3668781
zrydnoob <2642935611@qq.com>
2024-07-27 12:54:58 +0800

    fix(addItem.js): 🐛 修复registry id

--- startup_scripts/addItem.js
+++ startup_scripts/addItem.js
@@ -1,4 +1,4 @@
-StartupEvents.registry(event => {
+StartupEvents.registry('item',event => {
     event.create('math_paper').displayName('Math Paper')
     event.create('black_board').displayName('Black Board')
 })
